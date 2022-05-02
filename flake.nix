b6da8013695d8fdcb7ac23b613ba8e3aaaaa1d24
Matthew Toohey <contact@mtoohey.com>
2022-05-02 11:44:29 -0400

    build(flake): use packages.default instead of defaultPackage

--- flake.nix
+++ flake.nix
@@ -9,18 +9,17 @@
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem
       (system:
-        with import nixpkgs { inherit system; }; rec {
-          packages.git-crypt-agessh = buildGoModule rec {
+        with import nixpkgs { inherit system; }; {
+          packages.default = buildGoModule rec {
             pname = "git-crypt-agessh";
             name = pname;
             version = "0.1.0";
             src = ./.;
             vendorSha256 = "kxPxzVsn3bm5FNXFirEjUT5Sq/SLs+anhDBeyX63Vj0=";
           };
-          defaultPackage = packages.git-crypt-agessh;
 
           devShell = mkShell { nativeBuildInputs = [ go gopls ]; };
         }) // {
-      overlay = (final: prev: { git-crypt-agessh = self.defaultPackage."${prev.system}"; });
+      overlay = (final: _: { git-crypt-agessh = self.packages."${final.system}".default; });
     };
 }
